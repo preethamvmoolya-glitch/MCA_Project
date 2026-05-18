0d6587106c771ab15cfd3769443288f00e49e21c
rikhiladyar-dot <rikhiladyar@gmail.com>
2026-05-18 13:35:40 +0530

    Fix blank screen issue by using relative base path and adding .nojekyll

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MCA_Project/',
+  base: './',
   build: {
     outDir: '../docs',
     emptyOutDir: true
